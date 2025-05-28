1464c198cb5b6597375a2bf5eb85386144a962e1
andrius.antipovas <andrius.antipovas@visma.com>
2025-05-28 18:30:28 +0300

    init

---
+++ src/main/java/origamis/springframework/beerservice/OrigamiBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package origamis.springframework.beerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrigamiBeerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrigamiBeerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/origamis/springframework/beerservice/OrigamiBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package origamis.springframework.beerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrigamiBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
