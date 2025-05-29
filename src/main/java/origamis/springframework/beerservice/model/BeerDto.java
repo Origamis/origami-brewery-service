fd966fb24cc3ba1df9fc21e055c3895f4b99af3d
andrius.antipovas <andrius.antipovas@visma.com>
2025-05-30 00:06:01 +0300

    JSON format

--- src/main/java/origamis/springframework/beerservice/model/BeerDto.java
+++ src/main/java/origamis/springframework/beerservice/model/BeerDto.java
@@ -1,5 +1,6 @@
 package origamis.springframework.beerservice.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Null;
@@ -29,9 +30,11 @@ public class BeerDto {
     private String beerName;
     
     @Null
+    @JsonFormat(pattern ="yyyy-MM-dd'T'HH:mm:ssz", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
     
     @Null
+    @JsonFormat(pattern ="yyyy-MM-dd'T'HH:mm:ssz", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime lastModifiedDate;
     
     @NotNull
@@ -43,6 +46,7 @@ public class BeerDto {
     
     @NotNull
     @Positive
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private BigDecimal price;
     
     private Integer quantityOnHand;
